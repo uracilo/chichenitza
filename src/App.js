380cf3c8c32eb47e136375ed3ab7145046c06e12
Benjamin <benjamin.casazza@grupohuman.com>
2019-11-12 21:35:36 -0600

    :rocket:

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import ReactCompareImage from 'react-compare-image';
+import Camera from 'react-html5-camera-photo';
+import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ReactCompareImage
-          leftImage="https://i.pinimg.com/564x/6f/f5/28/6ff528f00d01efc8d5befdda1754e224.jpg"
-          rightImage="https://i.all3dp.com/cdn-cgi/image/fit=cover,w=1284,h=722,gravity=0.5x0.5,format=auto/wp-content/uploads/2018/12/28144052/background-images-can-come-in-handy-when-modeling-tian-ooi-all3dp-181228.jpg"
+
+
+
+class App extends Component {
+  onTakePhoto (dataUri) {
+    console.log('takePhoto');
+  }
+ 
+  render () {
+    return (
+      <div className="App">
+      
+      <ReactCompareSlider
+        itemOne={<ReactCompareSliderImage src="https://i.pinimg.com/564x/6f/f5/28/6ff528f00d01efc8d5befdda1754e224.jpg" alt="Image one" />}
+        itemTwo={<Camera />}
+      />
+    <div style={{display: "flex",  width: "100%",  height: "50vh", overflow: "hidden" }}  >
+        <ReactCompareSlider
+          style={{ display: "flex" }}
+          handle={
+            <div
+              style={{
+                backgroundImage: "linear-gradient(to top, red, blue)",
+                width: "2rem",
+                height: "100%"
+              }}
+            />
+          }
+          itemOne={
+            <ReactCompareSliderImage
+              src="https://i.pinimg.com/564x/6f/f5/28/6ff528f00d01efc8d5befdda1754e224.jpg"
+              style={{ filter: "blur(25px)" }}
+              alt="one"
+            />
+          }
+          itemTwo={
+            <ReactCompareSliderImage
+              src="https://i.pinimg.com/564x/6f/f5/28/6ff528f00d01efc8d5befdda1754e224.jpg"
+              alt="two"
+            />
+          }
+          onChange={position => console.log(`Landscape position: ${position}`)}
         />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </div>
 
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
-
 
