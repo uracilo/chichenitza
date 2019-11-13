2f8afeda821b2344db1016351e5c3841cdade6c6
Benjamin <benjamin.casazza@grupohuman.com>
2019-11-12 18:28:32 -0600

    chichen 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <ReactCompareImage
-          leftImage="https://www.w3schools.com/w3css/img_lights.jpg"
+          leftImage="https://i.pinimg.com/564x/6f/f5/28/6ff528f00d01efc8d5befdda1754e224.jpg"
           rightImage="https://i.all3dp.com/cdn-cgi/image/fit=cover,w=1284,h=722,gravity=0.5x0.5,format=auto/wp-content/uploads/2018/12/28144052/background-images-can-come-in-handy-when-modeling-tian-ooi-all3dp-181228.jpg"
         />
         <img src={logo} className="App-logo" alt="logo" />
